7cc232448d13acd085f7f8a20d8ceaf55c74324d
Alec Bunnell <bunnella@carleton.edu>
2015-11-17 15:18:06 -0600

    removed old non-microedge code; now we only have one script that does stuff

--- imageRestoration.R
+++ imageRestoration.R
@@ -3,24 +3,30 @@
 # RAP
 # Output
 
-display <- function(img, caption = "", ColVals = (V/2+.5)) {
-  img <- -img/2+.5
-  image(img, col = gray(ColVals), frame = F, asp = 1, xaxt = "n", yaxt = "n", main = caption)
+library(bmp)
+library(compiler)
+enableJIT(3)
+
+display <- function(img, caption = "") {
+  image(img/2+.5, col=gray(V/2+.5), zlim=0:1, frame=F, asp=1, xaxt="n", yaxt="n", main=caption)
 }
 
-degrade <- function(original, noiseProb=.1, V = createV(1)) {
-  degraded <- original
-  for (i in 1:(R*C)) {
-    flip <- sample(c(T,F), 1, prob=c(noiseProb, 1-noiseProb))
-    degraded[i] <- ifelse(flip, sample(V,1), degraded[i])
-    
-  } 
-  degraded
+degrade <- function(original, perturb_percentage=.2) {
+  static_vector <- rnorm(R*C,0,perturb_percentage/2)
+  # 90% within two standard deviations
+  # which is +/- perturb_percentage
+  # which is perturb_percentage as percentage of dist between -1 and 1
+  random_noise <- matrix(static_vector, nrow=R, ncol=C)
+  # print(head(random_noise))
+  added_noise <- original+random_noise
+  #added_noise
+  m <- matrix(mapply(function(x) min(max(x,-1), 1),added_noise),nrow=R,ncol=C)
+  m
 }
 
 neighbors <- function(i) {
 	m <- ((i-1) %% R) + 1
-	n <- ceiling(i / R) 
+	n <- ceiling(i / R)
 	return(neighborsHelp(m, n))
 }
 
@@ -43,7 +49,7 @@ neighborsHelp <- function(m, n) {
 
 convertItoRC <- function(i) {
 	m <- ((i-1) %% R) + 1
-	n <- ceiling(i / R) 
+	n <- ceiling(i / R)
 	return(c(m,n))
 }
 
@@ -56,58 +62,86 @@ d <- function(xs, ys) {
 	return((xs-ys)^2)
 }
 
-f <- function(xs,xt) {
-	return((xs-xt)^2)
+f <- function(xs, xt, e) {
+	return((xs-xt)^2*(1-e) + gamma*e)
 }
 
-createV <- function(num) {
-	seq(-1,1,length.out = num)
+# energy function evaluated when x_s = v (only over neighbors of s)
+H <- function(s, v) {
+  theta*d(v, y[s]) + sum(sapply(neighbors(s), function(t) f(v, x[t], getME(s, t))))
 }
 
 # returns a sample from the local characteristic distribution
 sampleXs <- function(s, beta = 1) {
-	probs <- sapply(V, function(v)
-		exp(-beta*(theta*d(v,y[s]) + sum(sapply(neighbors(s), function(t)
-			f(v,x[t])
-		))))
-	)
+	probs <- sapply(V, function(v) exp(-beta*H(s, v)))
 	sample(V, 1, prob = probs)
 }
 
+# returns a sampled microedge parity between pixels s & t
+sampleME <- function(s, t, beta = 1) {
+  probs <- sapply(0:1, function(e) exp(-beta*f(x[s], x[t], e)))
+  sample(0:1, 1, prob = probs) # TODO: try without beta?
+}
 
-#R <- C <- 20
-noiseProb <- .2
-theta <- 2
-N <- 10^2
-V <- createV(256)
+##############################################################################
+## Gibbs Sampler!
 
-#blah <- c(rep(-1, times=20), rep(c(-1,1,1,1,-1),times=12), rep(c(-1, rep(1,times=8), -1),times=2))
-#original <- matrix(rep(c(blah, rev(blah)), each=2), C, R)
+N <- 100 # number of sweeps
+V <- seq(-1, 1, length.out = 16) # set of discrete gray levels
+theta <- 4 # weight on data term
+gamma <- .1 # microedge penalty
 
-library(bmp)
-picture <- read.bmp("hi.bmp")
-values <- picture[,,1]
-values <- rbind(apply(cbind(apply(values, 2, rev)), 1, I))
-original <- values
-R <- nrow(original)
-C <- ncol(original)
+# read in the test image
+picture <- read.bmp("img/medium_cat.bmp")
+R <- ncol(picture)
+C <- nrow(picture)
 
-y <- degrade(original, noiseProb, V)
+# transform to image()-ready orientation and degrade
+ch.R <- t(picture[C:1, 1:R, 1])
+ch.G <- t(picture[C:1, 1:R, 2])
+ch.B <- t(picture[C:1, 1:R, 3])
+values <- 0.30*ch.R + 0.59*ch.G + 0.11*ch.B
+original <- values / 127.5 - 1 # scale from [0..255] -> [-1, 1]
+y <- degrade(original, perturb_percentage=.2)
 
-x <- y
-
+# plot original + degraded, leave room for MAP estimate
 par(mfrow = c(1, 3), mar = c(2.6, 1, 2.6, 1))
 display(original, "Original")
 display(y, "Noisy data")
 
-# Gibbs time ;)
-for (n in 1:N) {
-  for (s in 1:(R*C)) {
-    x[s] = sampleXs(s, min(exp((n - N/2)/20), 50))
-  }
+# data structures/functions for manipulating microedges
+eEW <- matrix(0, R, C-1)
+eNS <- matrix(0, R-1, C)
+getME <- function(s1, s2) {
+  r1 <- convertItoRC(s1)[1]; r2 <- convertItoRC(s2)[1];
+  c1 <- convertItoRC(s1)[2]; c2 <- convertItoRC(s2)[2];
+  if (r1 == r2 && abs(c1 - c2) == 1) eEW[r1, min(c1, c2)] else
+    if (c1 == c2 && abs(r1 - r2) == 1) eNS[min(r1, r2), c1] else
+      stop("invalid neighbors, stupid")
+}
+setME <- function(s1, s2, e) {
+  r1 <- convertItoRC(s1)[1]; r2 <- convertItoRC(s2)[1];
+  c1 <- convertItoRC(s1)[2]; c2 <- convertItoRC(s2)[2];
+  if (r1 == r2 && abs(c1 - c2) == 1) eEW[r1, min(c1, c2)] <<- e else
+    if (c1 == c2 && abs(r1 - r2) == 1) eNS[min(r1, r2), c1] <<- e else
+      stop("invalid neighbors, stupid")
 }
 
-display(x, "MAP")
+# Gibbs time ;)
+x <- y # init with degraded (given) image
+for (n in 1:N) {
+  beta = 45 # no annealing for now - was min(exp((n - N/2)/20), 50)
+  for (s in 1:(R*C)) {
+    x[s] = sampleXs(s, beta)
+  }
+  for (s in 1:(R*C)) {
+    for (t in neighbors(s)) {
+      if (t > s) {
+        setME(s, t, sampleME(s, t, beta))
+      }
+    }
+  }
+  cat(paste0("\r", round(100*n/N), "%\r"))
+}
 
-
-
+display(x, "MAP estimate")
